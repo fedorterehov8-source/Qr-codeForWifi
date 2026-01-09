30093773ff4d4253a747f9608942f2c1c8a51f61
Fxxxr <fedorterehov8@gmail.com>
2026-01-09 13:41:08 +0300

    qr

---
+++ qr/QRcodeForWifi.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using QRCoder;
+var wifi1 = new PayloadGenerator.WiFi(
+    ssid: "Введите название сети",
+    password: "Пароль от сетии",
+    authenticationMode: PayloadGenerator.WiFi.Authentication.WPA);
+var generator = new QRCodeGenerator();
+var data = generator.CreateQrCode(wifi1.ToString(), QRCodeGenerator.ECCLevel.Q);
+var qrCode = new PngByteQRCode(data);
+byte[] pngBytes = qrCode.GetGraphic(20);
+File.WriteAllBytes("wifi.png", pngBytes);
